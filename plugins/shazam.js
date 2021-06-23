f1ae7252452f5e4b69dd4b7f01cabe27d68d7a8d
Afnan PLK <83832372+afnanplk@users.noreply.github.com>
2021-06-23 15:09:51 +0530

    Update shazam.js

--- plugins/shazam.js
+++ plugins/shazam.js
@@ -5,36 +5,38 @@ const fs = require('fs');
 const axios = require('axios');
 const FormData = require('form-data');
 const ffmpeg = require('fluent-ffmpeg');
+const ytdl = require('ytdl-core');
+const yts = require( 'yt-search' )
+const got = require("got");
+const ID3Writer = require('browser-id3-writer');
+const SpotifyWebApi = require('spotify-web-api-node');
+const req = require('request');
 
-Asena.addCommand({pattern: 'find', fromMe: false, desc: 'Shazam plugin'}, (async (message, match) => {
-    if (message.reply_message === false) return await message.client.sendMessage(message.jid, 'replay to a audio!', MessageType.text);
-    var filePath = await message.client.downloadAndSaveMediaMessage({
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3',
+    clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009'
+});
+
+Asena.addCommand({pattern: 'sha', fromMe: true, desc: 'sh' }, (async (message, match) => { 
+
+    if (message.reply_message === false) return await message.client.sendMessage(message.jid, bix.UV_REPLY, MessageType.text);
+
+    var location = await message.client.downloadAndSaveMediaMessage({
         key: {
             remoteJid: message.reply_message.jid,
             id: message.reply_message.id
         },
         message: message.reply_message.data.quotedMessage
     });
-    var form = new FormData();
-    ffmpeg(filePath).format('mp3').save('music.mp3').on('end', async () => {
-        form.append('api_token', '2bd1fcb3ccd15607f72cdbb527907ce6');
-        form.append('file', fs.createReadStream('./music.mp3'));
-        form.append('return', 'apple_music, spotify');
-        var configs = {
-            headers: {
-                ...form.getHeaders()
-            }
-        }
-        await axios.post('https://api.audd.io/', form, configs).then(async (response) => {
-            var res = response.data
-            if (res === 'success') {
-                await message.client.sendMessage(message.jid, `Title: ${res.title}\nArtist: ${res.artist}`, MessageType.text);
-            } else {
-                await message.client.sendMessage(message.jid, 'No results found', MessageType.text);
-            }
-        }).catch((error) =>  {
-            console.log(error);
-        });
-    });
 
+    ffmpeg(location)
+        .format('mp3')
+        .save('lyr.mp3')
+        .on('end', async () => {
+
+            var data = { 'api_token': '6a03ebccfc18bb19e3fb7bb5280a51ab', 'file': fs.createReadStream('./lyr.mp3'), 'return': 'apple_music,spotify' };
+            req ({ uri: 'https://api.audd.io/', form: data, method: "POST" }, async (err, res, body) => {
+                return await message.client.sendMessage(message.jid, body, MessageType.text);
+            })
+        })
 }));
